5a2e41701a7c4a8c6b59d6331c8187fc0ce74d9b
Varun <40916057+Prais3@users.noreply.github.com>
2019-01-27 10:43:15 -0500

    Create doublestack.cxx

---
+++ doublestack.cxx
@@ -0,0 +1,44 @@
+//---------------------------------------------------------------------------------------------------------------
+// Name: Varun Jani
+// 
+// Description: This program asks us to create or implement four files (charstack, doublestack, charlist, doublelist) 
+// and then convert infix to postfix and then evaluate the value of the postfix expression. 
+// Once all the test cases are passed it will show congratulations, all the test cases are passed
+//---------------------------------------------------------------------------------------------------------------
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <cassert>
+#include "doublestack.h"
+
+using namespace std;
+
+bool DoubleStack::IsEmpty() const
+{
+    return list.IsEmpty();
+}
+
+void DoubleStack::Push(double elem)
+{
+    list.AddToHead(elem);
+}
+
+double DoubleStack::Top() const
+{
+    return list.GetHead();
+    
+}
+
+double DoubleStack::Pop()
+{
+    double t = Top();
+    list.RemoveHead();
+    return t;
+}
+
+void DoubleStack::Clear()
+{
+    while(IsEmpty() == false)
+      Pop();
+}
